1a750baf581358d9f6bdcedd91a9fe6112d12c9d
GrayZeus <marco.hofacker@gmx.de>
2023-02-08 17:32:28 +0100

    Implementation completed, idea with Boarder Guard works good. Next step: Testing

---
+++ src/main/java/BorderGuard.java
@@ -0,0 +1,13 @@
+public class BorderGuard {
+
+
+    public Barrier openBarrier(Barrier barrier){
+        barrier.open();
+        return barrier;
+    }
+
+    public Barrier closeBarrier(Barrier barrier){
+        barrier.close();
+        return barrier;
+    }
+}
--- src/main/java/NormalTrack.java
+++ src/main/java/NormalTrack.java
@@ -1,13 +1,27 @@
 public class NormalTrack extends Track {
     // here no special constructor, last element
+    BorderGuard jeremy = new BorderGuard();
+
+    Barrier barrierNormalTrack = new Barrier();
+
     public void passBorder(Vehicle vehicle){
         //System.out.println("Normal Track Class: " + vehicle.getClass().toString());
         if(canHandleVehicle(vehicle, "class Car")){
             System.out.println("Boarder guard is checking for personal documents and checks the vehicle");
+            System.out.println("Boarder guard opens the barrier");
+            barrierNormalTrack = jeremy.openBarrier(barrierNormalTrack);
+            outputBarrierState();
             System.out.println("NormalTrack: Vehicle passed");
+            System.out.println("Boarder guard closes the barrier");
+            barrierNormalTrack = jeremy.closeBarrier(barrierNormalTrack);
+            outputBarrierState();
         }
         else{
             super.passBorder(vehicle);
         }
+    }
+
+    public void outputBarrierState(){
+        System.out.println(barrierNormalTrack.toString() + " state: " + barrierNormalTrack.getState());
     }
 }
